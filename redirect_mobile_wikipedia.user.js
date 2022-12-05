5d894b4479e63c553d3410f6cfc1dea1ee789dd6
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-05 16:37:21 +0800

    Add files via upload

---
+++ redirect_mobile_wikipedia.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         RedirectMobileWikipedia
+// @name:zh-CN   Wikipedia 移动端重定向
+// @match        https://zh.m.wikipedia.org/*
+// @version      0.3.3
+// @description  Force redirect Chinese Wikipedia from mobile version to desktop version.
+// @description:zh-CN  将中文 Wikipedia 移动端页面重定向至桌面端页面。
+// @author       Moe
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  let url = window.location.href;
+  let desturl = url;
+  if (url.search("zh.m.wikipedia.org") !== -1) {
+    desturl = url.replace("m.", "");
+  }
+  window.location.replace(desturl);
+})();
