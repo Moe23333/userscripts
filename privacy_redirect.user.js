c3998dd5a9e60c51afeca0ea61d84416b1d8df09
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-07 18:37:37 +0800

    Update privacy_redirect.user.js

--- privacy_redirect.user.js
+++ privacy_redirect.user.js
@@ -2,9 +2,10 @@
 // @name         PrivacyRedirect
 // @name:zh-CN   隐私重定向
 // @match        *://*.youtube.com/*
+// @match        *://*.youtu.be/*
 // @match        *://*.twitter.com/*
 // @match        *://*.reddit.com/*
-// @version      0.3.3
+// @version      0.3.4
 // @description  Redirect YouTube, Twitter, Reddit to their privacy-friendly front-end alternative.
 // @description:zh-CN  将 YouTube、Twitter、Reddit 重定向至它们的隐私友好型前端替代品。
 // @author       Moe
