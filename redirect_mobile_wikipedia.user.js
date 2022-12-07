f7c168bb3aa469b22e09f1019c58e177232a3e3b
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-07 18:46:17 +0800

    Update redirect_mobile_wikipedia.user.js

--- redirect_mobile_wikipedia.user.js
+++ redirect_mobile_wikipedia.user.js
@@ -2,7 +2,7 @@
 // @name         RedirectMobileWikipedia
 // @name:zh-CN   Wikipedia 移动端重定向
 // @match        https://zh.m.wikipedia.org/*
-// @version      0.3.4
+// @version      0.3.5
 // @description  Force redirect Chinese Wikipedia from mobile version to desktop version.
 // @description:zh-CN  将中文 Wikipedia 移动端页面重定向至桌面端页面。
 // @author       Moe
@@ -12,10 +12,9 @@
 
 (function () {
   "use strict";
-  let url = window.location.href;
-  let desturl = url;
-  if (url.search("zh.m.wikipedia.org") !== -1) {
-    desturl = url.replace("m.", "");
+  let newURL;
+  if (window.location.hostname == "zh.m.wikipedia.org") {
+    newURL = url.replace("m.", "")
   }
-  window.location.replace(desturl);
+  window.location.replace(newURL);
 })();
