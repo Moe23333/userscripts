3a6758309cdbc9ababa56a106f5d45fed835e890
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-07 14:38:00 +0800

    Update privacy_redirect.user.js

--- privacy_redirect.user.js
+++ privacy_redirect.user.js
@@ -4,7 +4,7 @@
 // @match        *://*.youtube.com/*
 // @match        *://*.twitter.com/*
 // @match        *://*.reddit.com/*
-// @version      0.3.1
+// @version      0.3.2
 // @description  Redirect YouTube, Twitter, Reddit to their privacy-friendly front-end alternative.
 // @description:zh-CN  将 YouTube、Twitter、Reddit 重定向至它们的隐私友好型前端替代品。
 // @author       Moe
@@ -19,11 +19,14 @@
   if (url.search("www.youtube.com") !== -1) {
     desturl = url.replace("www.youtube.com", "piped.video");
   }
+  if (url.search("m.youtube.com") !== -1) {
+    desturl = url.replace("m.youtube.com", "piped.video");
+  }
+  if (url.search("https://twitter.com") !== -1) {
+    desturl = url.replace("https://twitter.com", "https://twiiit.com");
+  }
   if (url.search("mobile.twitter.com") !== -1) {
     desturl = url.replace("mobile.twitter.com", "twiiit.com");
-  }
-  if (url.search("twitter.com") !== -1) {
-    desturl = url.replace("twitter.com", "twiiit.com");
   }
   if (url.search("www.reddit.com") !== -1) {
     desturl = url.replace("www.reddit.com", "libreddit.spike.codes");
