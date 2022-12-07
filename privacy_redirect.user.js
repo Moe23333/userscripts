5be4e6d6980c7d179a909ac4a44622613ac29572
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-07 18:36:39 +0800

    Update privacy_redirect.user.js

--- privacy_redirect.user.js
+++ privacy_redirect.user.js
@@ -4,7 +4,7 @@
 // @match        *://*.youtube.com/*
 // @match        *://*.twitter.com/*
 // @match        *://*.reddit.com/*
-// @version      0.3.2
+// @version      0.3.3
 // @description  Redirect YouTube, Twitter, Reddit to their privacy-friendly front-end alternative.
 // @description:zh-CN  将 YouTube、Twitter、Reddit 重定向至它们的隐私友好型前端替代品。
 // @author       Moe
@@ -14,22 +14,19 @@
 
 (function () {
   "use strict";
-  let url = window.location.href;
-  let desturl = url;
-  if (url.search("www.youtube.com") !== -1) {
-    desturl = url.replace("www.youtube.com", "piped.video");
+  let URL = window.location.hostname;
+  let TwitterURLs = ["twitter.com", "mobile.twitter.com"];
+  let YouTubeURLs = ["www.youtube.com", "m.youtube.com", "youtu.be"];
+  let RedditURLs = ["www.reddit.com"];
+  let newURL;
+  if (TwitterURLs.includes(URL)) {
+    newURL = "https://twiiit.com" + window.location.pathname;
   }
-  if (url.search("m.youtube.com") !== -1) {
-    desturl = url.replace("m.youtube.com", "piped.video");
+  if (YouTubeURLs.includes(URL)) {
+    newURL = "https://piped.video" + window.location.pathname;
   }
-  if (url.search("https://twitter.com") !== -1) {
-    desturl = url.replace("https://twitter.com", "https://twiiit.com");
+  if (RedditURLs.includes(URL)) {
+    newURL = "https://libreddit.spike.codes" + window.location.pathname;
   }
-  if (url.search("mobile.twitter.com") !== -1) {
-    desturl = url.replace("mobile.twitter.com", "twiiit.com");
-  }
-  if (url.search("www.reddit.com") !== -1) {
-    desturl = url.replace("www.reddit.com", "libreddit.spike.codes");
-  }
-  window.location.replace(desturl);
+  window.location.replace(newURL);
 })();
