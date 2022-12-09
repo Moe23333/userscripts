021218859d4921621c00b8b1f5f90cb84f40a293
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-10 02:03:40 +0800

    Update privacy_redirect.user.js

--- privacy_redirect.user.js
+++ privacy_redirect.user.js
@@ -8,7 +8,7 @@
 // @match        *://*.reddit.com/*
 // @match        *://*.redd.it/*
 // @match        *://translate.google.com/*
-// @version      0.4.0
+// @version      0.4.1
 // @description  Redirect YouTube, Twitter, Reddit, Google Translate to their privacy-friendly front-end alternative.
 // @description:zh-CN  将 YouTube、Twitter、Reddit、Google 翻译重定向至它们的隐私友好型前端替代品。
 // @author       Moe
