e6ac5a7ef125074da3d2deef70b510d41b176fa0
Moe <35591595+Moe23333@users.noreply.github.com>
2022-12-11 02:09:11 +0800

    Update privacy_redirect.user.js

--- privacy_redirect.user.js
+++ privacy_redirect.user.js
@@ -8,7 +8,7 @@
 // @match        *://*.reddit.com/*
 // @match        *://*.redd.it/*
 // @match        *://translate.google.com/*
-// @version      0.4.1
+// @version      0.4.2
 // @description  Redirect YouTube, Twitter, Reddit, Google Translate to their privacy-friendly front-end alternative.
 // @description:zh-CN  将 YouTube、Twitter、Reddit、Google 翻译重定向至它们的隐私友好型前端替代品。
 // @author       Moe
@@ -18,22 +18,24 @@
 
 (function () {
   "use strict";
-  let URL = window.location.hostname;
   let TwitterURLs = ["twitter.com", "mobile.twitter.com"];
   let YouTubeURLs = ["www.youtube.com", "m.youtube.com", "www.youtube-nocookie.com", "youtu.be"];
   let RedditURLs = ["www.reddit.com", "redd.it", "v.redd.it"];
   let GTranslateURLs = ["translate.google.com"];
   let newURL;
-  if (TwitterURLs.includes(URL)) {
+  if (window.location.hostname + window.location.pathname === "twitter.com/i/redirect") {
+    newURL = "http://twiiit.com"+ new URL(decodeURIComponent(window.location.search.replace("?url=", ""))).pathname; 
+  }
+  else if (TwitterURLs.includes(window.location.hostname)) {
     newURL = "https://twiiit.com" + window.location.pathname;
   }
-  if (YouTubeURLs.includes(URL)) {
+  else if (YouTubeURLs.includes(window.location.hostname)) {
     newURL = "https://piped.video" + window.location.pathname + window.location.search;
   }
-  if (RedditURLs.includes(URL)) {
+  else if (RedditURLs.includes(window.location.hostname)) {
     newURL = "https://libreddit.spike.codes" + window.location.pathname;
   }
-  if (GTranslateURLs.includes(URL)) {
+  else if (GTranslateURLs.includes(window.location.hostname)) {
     newURL = "https://simplytranslate.org" + window.location.pathname;
   }
   window.location.replace(newURL);
